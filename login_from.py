5edd860e6dbf160df0206603ce5376a1d32fb321
Shivaram <shivarambussa3008@gmail.com>
2026-01-29 15:15:24 +0530

    Update login_from.py

--- login_from.py
+++ login_from.py
@@ -1,131 +1,140 @@
 import streamlit as st
 
-st.header("b.shivaram ")
+#header.
+st.header("Anurag university Student Records Management")
+#title of the app
 
-#title
-st.title("welcome")
+st .title("welcome to student records management system")
+#subheader of the app
+st.subheader("Mang student efficiently and effectively")
 
-#subheader
-st.subheader("login now")
+#text method to display information
 
-#horizantal line
-st.markdown("---------------------------------------------------------")
+st.text("Hii,I am krishna Anand Chauhan ")
 
-st.text(" this app is to allow u to make ")
+#horizontal line
 
-st.write("Hello shivaram")
-st.write(234)
-#st.write(we can use dict also)
+st.markdown("----------------------------")
 
-st.markdown("### ###")
-st.markdown("**bold text**")
-st.markdown("*italic one*")
-st.markdown("- team1 \n- team2")
+#write method--allows to write something
 
-st.caption("shivaram this is caption")
+st.write(" hello krishna")
+st.write(123456)
+st.write(1,2,3,4,5)
+
+# st.write("name""krishna","role":"student")
+st.markdown("### this is markdown")
+st.markdown("**Bold text**")
+st.markdown("*Italic*")
+st.markdown("-item 1\n item 2-")
+
+st.markdown("<h3 style=color:red>RED TEXT </h3>",unsafe_allow_html=True)
+
 st.code("""
-def add(a,b):
-        return a+b
-        """, language="python")
+        def add(a,b):
+            return a+b
+       
+        """,language='python')
 
-# latex is used to display math functions
+#latex
+st.latex(r''' a^2 + b^2 = c^2 ''')
 
-st.latex(r'''
-a^2 + b^2 = c^2''')
-
-#divider method is used to separate sessions
+#divider
 st.divider()
 
-#button
-if st.button("click done"):
-    st.write("ur done!")
+if st.button("click me"):
+    st.success("button clicked")
     st.balloons()
+    st.snow()
 else:
-    st.error("conn error!")
-st.divider()
+    st.error("button not clicked")
 
-# input num
-id = st.text_input("Name: " ) 
-if st.button("done"):
-    if id=="":
-        st.warning("name cannot be empty")
-    elif not id.isalpha() :
-        st.error("invalid input")
-    else:
-        st.write(f"hello{id}")       
-st.divider()
-feedback=st.text_area("give feedback:")
-st.divider()
-if st.checkbox(" i agree "):
-    st.write("thank u for agreeing")
-st.divider()
+#text input
+name=st.text_input("enter your name")
+# st.write("your name is:",name)
 
-gen = st.radio("Choose Operation", [" male", " female"," other", " Read"])
-st.write(f"your choice :{gen}")
-st.divider()
-y=st.selectbox("select ur country:",("ind","aus","dub"))
-st.write(f" u selected: { y}")
-st.divider()
-#slider method to create a slider
-age = st.slider("select your age:", 0,100,25)
-st.write(f"ur {age}")
-st.divider()
-#to upload file use "file_uploader()"
+if name ==" ":
+    st.warning("Name cannot be empty")
+elif not name.isalpha():
+    st.error("Name should contain only alphabets no numbers allowed")
+else:
+    st.success(f"Welcome {name} to Anurag University")
+   
+#number input
+age=st.number_input("enter your age",min_value=1,max_value=100,step=1)
+st.write("your age is:",age)
 
-up= st.file_uploader("choose a flie")
-if up is not None:
-    st.success("file up success!")
-    st.write(f"filename:")
+#mutli line text input
+address=st.text_area("enter your address")
+st.write("your address is:",address)
 
-st.divider()
+st.checkbox("I agree to the terms and conditions")
+st.radio("select your gender",("male","female"))
+country=st.selectbox("select your country",("india","usa","uk","canada"))
+st.write("your country is:",country)
+hobbies=st.multiselect("select your hobbies",("reading","traveling","coding","sports"))
+st.write("your hobbies are:",hobbies)
+st.slider("select your age :",0,100,25)
 
-#form method
+uploaded_file=st.file_uploader("choose a file")
+if uploaded_file is not None:
+    st.success("file uploaded successfully")
+    st.write("filename:",uploaded_file.name)
+
+
+
+
 with st.form("my_form"):
-    name= st.text_input("Name: ")
-    age=st.number_input("age:")
+    st.text_input("enter your email")
+    st.number_input("enter your age",min_value=1,max_value=100,step=1)
     submit=st.form_submit_button("submit")
-
 if submit:
-    st.balloons()
-    st.write("ur done!")
-st.divider()
-
-with st.form("login_form"):
-    id= st.text_input("username: ")
-    password=st.text_input("pass:",type="password")
+    st.write("name:",name,"age:",age)
+    st.success("form submitted successfully")
+#form submission
+with st.form("login"):
+    name=st.text_input("enter your email")
+    age= st.number_input("enter your age",min_value=1,max_value=100,step=1)
     login=st.form_submit_button("login")
-
 if login:
-    st.balloons()
-    st.write("ur done!")   
-
+    st.write("name:",name,"age:",age)
+    st.success("form submitted successfully")
 st.divider()
-#col method 
-
+#columns
 col1,col2,col3=st.columns(3)
 with col1:
-    st.subheader("col1")
-    st.write("this is col1")    
+    st.header("column 1")
+    st.text("this is column 1")
 with col2:
-    st.subheader("col2")
-    st.write("this is col2") 
-
+    st.header("column 2")
+    st.text("this is column 2")
 with col3:
-    st.subheader("col3")
-    st.write("this is col3")         
+    st.header("column 3")
+    st.text("this is column 3")
+
 st.divider()
-    
+#container
+container=st.container()
+container.write("this is inside the container")
+container.button("click")
+
 data = {
     'Name': ['Anurag', 'Sumit', 'Rohit'],
     'Age': [21, 22, 20],
     'Course': ['B.Tech', 'M.Tech', 'BBA']
 }
-st.table(data)    
+st.table(data)
+
 st.divider()
 
-st.sidebar.title("Menu")
-option = st.sidebar.selectbox(
-"Choose page",
-["Home", "About", "Contact"]
-)
-st.sidebar.write(f"You selected: {option}")
+#sidebar
+st.sidebar.title("sidebar title")
+option=st.sidebar.selectbox("select an option",("option 1","option 2","option 3"))
+st.sidebar.write("you selected:",option)
+
+
+@st.cache_data
+def laod_data():
+    return [1,2,3,4]
+data=laod_data()
+st.write(data)
